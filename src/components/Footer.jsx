072d744ad3c0086b84180decdc0a84bbaac61798
EnricoCiccolini <piroroma90@gmail.com>
2025-04-03 19:02:01 +0200

    feat: fix font and hover

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,36 +10,36 @@ export default function Footer() {
 
                                 <ul>
                                     <li><h3>DC COMICS</h3></li>
-                                    <li>Characters</li>
-                                    <li>Comics</li>
-                                    <li>Movies</li>
-                                    <li>Tv</li>
-                                    <li>Gasmes</li>
-                                    <li>Video</li>
-                                    <li>News</li>
+                                    <li className="opacity5">Characters</li>
+                                    <li className="opacity5">Comics</li>
+                                    <li className="opacity5">Movies</li>
+                                    <li className="opacity5">Tv</li>
+                                    <li className="opacity5">Gasmes</li>
+                                    <li className="opacity5">Video</li>
+                                    <li className="opacity5">News</li>
                                 </ul>
 
                                 <ul>
                                     <li><h3>SHOP</h3></li>
-                                    <li>Shop DC</li>
-                                    <li>Shop Dc Collectibles</li>
+                                    <li className="opacity5" >Shop DC</li>
+                                    <li className="opacity5">Shop Dc Collectibles</li>
                                 </ul>
                             </div>
                             <div>
 
                                 <ul>
                                     <li><h3>DC</h3></li>
-                                    <li>Term Of Use</li>
-                                    <li>Privacy policy(New)</li>
-                                    <li>Ad Choices</li>
-                                    <li>Advertising</li>
-                                    <li>Jobs</li>
-                                    <li>Subscriptions</li>
-                                    <li>Talent Workshops</li>
-                                    <li>CPSC certificates</li>
-                                    <li>Ratings</li>
-                                    <li>Shop Help</li>
-                                    <li>Contact Us</li>
+                                    <li className="opacity5">Term Of Use</li>
+                                    <li className="opacity5">Privacy policy(New)</li>
+                                    <li className="opacity5">Ad Choices</li>
+                                    <li className="opacity5">Advertising</li>
+                                    <li className="opacity5">Jobs</li>
+                                    <li className="opacity5">Subscriptions</li>
+                                    <li className="opacity5">Talent Workshops</li>
+                                    <li className="opacity5">CPSC certificates</li>
+                                    <li className="opacity5">Ratings</li>
+                                    <li className="opacity5">Shop Help</li>
+                                    <li className="opacity5">Contact Us</li>
                                 </ul>
                             </div>
 
@@ -47,11 +47,11 @@ export default function Footer() {
 
                                 <ul>
                                     <li><h3>SITES</h3></li>
-                                    <li>DC</li>
-                                    <li>MAD Magazine</li>
-                                    <li>DC Kids</li>
-                                    <li>DC Universe</li>
-                                    <li>Dc Power Vista</li>
+                                    <li className="opacity5">DC</li>
+                                    <li className="opacity5">MAD Magazine</li>
+                                    <li className="opacity5">DC Kids</li>
+                                    <li className="opacity5">DC Universe</li>
+                                    <li className="opacity5">Dc Power Vista</li>
                                 </ul>
                             </div>
 
@@ -64,9 +64,9 @@ export default function Footer() {
                 <div className="bg-grey relative">
                     <div className="container">
                         <div className="flex space-beet item-center ">
-                            <button>SIGN UP NOW</button>
+                            <button className="ft-button">SIGN UP NOW</button>
                             <div className="flex item-center gap10 px-20 ">
-                                <h3>FOLLOW US</h3>
+                                <h3 className="tx-primary">FOLLOW US</h3>
                                 <img src="public\footer-facebook.png" alt="facebook" />
                                 <img src="public\footer-twitter.png" alt="twitter" />
                                 <img src="public\footer-youtube.png" alt="youtube" />
