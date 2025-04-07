727bfdfe9475923009074e13007ab0bc48e61248
EnricoCiccolini <piroroma90@gmail.com>
2025-04-07 15:04:46 +0200

    fix legibility card generator create

--- src/components/CardGenerator.jsx
+++ src/components/CardGenerator.jsx
@@ -2,13 +2,15 @@
 export default function CardGenerator({ array }) {
 
 
-
-
-
-
     return (
         <>
-            {array.map((l, i) => <div className='card' key={l.id} ><div ><img className='imagecard' src={l.thumb} alt={l.series} /></div><h2>{l.series}</h2></div>
+            {array.map((l, i) => (
+                <div className='card' key={l.id} >
+                    <div >
+                        <img className='imagecard' src={l.thumb} alt={l.series} />
+                    </div>
+                    <h2>{l.series}</h2>
+                </div>)
             )}
         </>
     )
